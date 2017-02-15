b576abae466ea290702c4cf37cbb7c28a09be59a
Scott Enderle <scott.enderle@gmail.com>
2017-02-15 15:40:57 -0500

    add link-making script

---
+++ cmd/links.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+ln -s $HOME/.bah/bash_aliases $HOME/.bash_aliases
+ln -s $HOME/.bah/bash_profile $HOME/.bash_profile
+ln -s $HOME/.bah/bashrc $HOME/.bashrc
+ln -s $HOME/.bah/pythonstartup $HOME/.pythonstartup
+ln -s $HOME/.bah/vim $HOME/.vim
+ln -s $HOME/.bah/vimrc $HOME/.vimrc
