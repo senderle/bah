4fd97a5ed4c3206594f58ebfaa3fb5a2a6d5c080
Scott Enderle <scott.enderle@gmail.com>
2017-04-06 14:41:06 +0000

    add dircolors functionality

--- cmd/links.sh
+++ cmd/links.sh
@@ -6,3 +6,4 @@ ln -s $HOME/.bah/bashrc $HOME/.bashrc
 ln -s $HOME/.bah/pythonstartup $HOME/.pythonstartup
 ln -s $HOME/.bah/vim $HOME/.vim
 ln -s $HOME/.bah/vimrc $HOME/.vimrc
+ln -s $HOME/.bah/dircolors $HOME/.dircolors
